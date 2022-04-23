f00f67bd598da45195cb7a321425d696440d5b97
vaniyodha <vaniyodha368@gmail.com>
2022-04-23 12:48:18 +0530

    selector

---
+++ index.js
@@ -0,0 +1,12 @@
+
+document.title = "dom"
+
+
+let text = document.getElementById("#onetime")
+console.log(text)
+let text2 = document.getElementsByClassName("read")
+console.log(text2)
+let texxt3 = document.querySelector("p")
+console.log(texxt3)
+let text4 = document.querySelectorAll("p")
+console.log(text4)
